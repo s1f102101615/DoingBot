90fffe2f7d503de3ac410c012bbe260737bdb390
s1f102101615 <s1f102101615@iniad.org>
2023-07-16 05:59:08 +0900

    !sleep,!doing,!wakeコマンドの実装

--- build/main.js
+++ build/main.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,6 +38,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //必要なパッケージをインポートする
 const discord_js_1 = require("discord.js");
 const dotenv_1 = __importDefault(require("dotenv"));
+const fs = __importStar(require("fs"));
 //.envファイルを読み込む
 dotenv_1.default.config();
 //Botで使うGetwayIntents、partials
@@ -37,12 +61,72 @@ client.once('ready', () => {
 });
 //!timeと入力すると現在時刻を返信するように
 client.on('messageCreate', (message) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     if (message.author.bot)
         return;
-    if (message.content === '!time') {
+    if (message.content === '!sleep') {
         const date1 = new Date();
+        const name = message.author.username;
         message.channel.send(date1.toLocaleString());
+        message.channel.send(name + "が寝ました！");
+        saveCommand(name);
+    }
+    if (message.content === '!doing') {
+        const savedUsers = yield getSavedUsers();
+        const allUsers = (_a = message.guild) === null || _a === void 0 ? void 0 : _a.members.cache.map(member => member.user.username);
+        const nonSleepers = allUsers === null || allUsers === void 0 ? void 0 : allUsers.filter(user => !savedUsers.includes(user));
+        message.channel.send('寝てない人: ' + (nonSleepers === null || nonSleepers === void 0 ? void 0 : nonSleepers.join(', ')));
+    }
+    if (message.content === '!wake') {
+        const name = message.author.username;
+        removeUserFromSleepList(name);
+        message.channel.send(name + 'が起きました！');
     }
 }));
+function saveCommand(command) {
+    fs.appendFile('sleep.txt', command + '\n', (err) => {
+        if (err) {
+            console.error('Error saving command:', err);
+        }
+        else {
+            console.log(command);
+        }
+    });
+}
+function getSavedUsers() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            fs.readFile('sleep.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Error reading saved users:', err);
+                    reject(err);
+                }
+                else {
+                    const savedUsers = data.split('\n').map(line => line.trim());
+                    resolve(savedUsers);
+                }
+            });
+        });
+    });
+}
+function removeUserFromSleepList(name) {
+    fs.readFile('sleep.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading saved users:', err);
+        }
+        else {
+            const savedUsers = data.split('\n').map(line => line.trim());
+            const updatedUsers = savedUsers.filter(user => user !== name);
+            fs.writeFile('sleep.txt', updatedUsers.join('\n'), 'utf8', (err) => {
+                if (err) {
+                    console.error('Error removing user from sleep list:', err);
+                }
+                else {
+                    console.log('User removed from sleep list!');
+                }
+            });
+        }
+    });
+}
 //ボット作成時のトークンでDiscordと接続
 client.login(process.env.TOKEN);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 //必要なパッケージをインポートする
 import { GatewayIntentBits, Client, Partials, Message } from 'discord.js'
 import dotenv from 'dotenv'
+import * as fs from 'fs';
 
 //.envファイルを読み込む
 dotenv.config()
@@ -28,11 +29,64 @@ client.once('ready', () => {
 //!timeと入力すると現在時刻を返信するように
 client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return
-    if (message.content === '!time') {
+    if (message.content === '!sleep') {
         const date1 = new Date();
+        const name = message.author.username;
         message.channel.send(date1.toLocaleString());
+        message.channel.send(name + "が寝ました！");
+        saveCommand(name);
+    }
+    if (message.content === '!doing') {
+      const savedUsers = await getSavedUsers();
+      const allUsers = message.guild?.members.cache.map(member => member.user.username);
+      const nonSleepers = allUsers?.filter(user => !savedUsers.includes(user));
+      message.channel.send('寝てない人: ' + nonSleepers?.join(', '));
+    }
+    if (message.content === '!wake') {
+      const name = message.author.username;
+      removeUserFromSleepList(name);
+      message.channel.send(name + 'が起きました！');
     }
 })
+function saveCommand(command: string) {
+  fs.appendFile('sleep.txt', command + '\n', (err) => {
+    if (err) {
+      console.error('Error saving command:', err);
+    } else {
+      console.log(command);
+    }
+  });
+}
+async function getSavedUsers(): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    fs.readFile('sleep.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading saved users:', err);
+        reject(err);
+      } else {
+        const savedUsers = data.split('\n').map(line => line.trim());
+        resolve(savedUsers);
+      }
+    });
+  });
+}
+function removeUserFromSleepList(name: string) {
+  fs.readFile('sleep.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading saved users:', err);
+    } else {
+      const savedUsers = data.split('\n').map(line => line.trim());
+      const updatedUsers = savedUsers.filter(user => user !== name);
+      fs.writeFile('sleep.txt', updatedUsers.join('\n'), 'utf8', (err) => {
+        if (err) {
+          console.error('Error removing user from sleep list:', err);
+        } else {
+          console.log('User removed from sleep list!');
+        }
+      });
+    }
+  });
+}
 
 //ボット作成時のトークンでDiscordと接続
 client.login(process.env.TOKEN)
